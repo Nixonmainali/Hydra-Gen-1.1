e739eedd50de3f6633040be6fc331f5f7e0a4a6f
knq1337 <99088896+knq1337@users.noreply.github.com>
2022-02-05 18:45:07 +0200

    Add files via upload

---
+++ hydra.py
@@ -0,0 +1,280 @@
+import os
+import colorama
+import time
+from colorama import Fore
+import random
+import requests
+import os
+import json
+import webbrowser
+import platform as plt
+from re import findall
+from base64 import b64decode 
+from datetime import datetime
+from json import loads, dumps
+from urllib.request import Request, urlopen
+
+colorama.init()
+
+print(Fore.YELLOW +'   THIS APP WAS MADE BY knq#1111   ')
+webbrowser.open('https://discord.gg/8XAkCEv8KU')
+time.sleep(3)
+
+def main1():
+
+  os.system('cls')
+
+  print('')
+  print(Fore.BLUE + '      ██╗  ██╗██╗   ██╗██████╗ ██████╗  █████╗      ██████╗ ███████╗███╗   ██╗')
+  print(Fore.BLUE + '      ██║  ██║╚██╗ ██╔╝██╔══██╗██╔══██╗██╔══██╗    ██╔════╝ ██╔════╝████╗  ██║')
+  print(Fore.BLUE + '      ███████║ ╚████╔╝ ██║  ██║██████╔╝███████║    ██║  ███╗█████╗  ██╔██╗ ██║')
+  print(Fore.BLUE + '      ██╔══██║  ╚██╔╝  ██║  ██║██╔══██╗██╔══██║    ██║   ██║██╔══╝  ██║╚██╗██║')
+  print(Fore.BLUE + '      ██║  ██║   ██║   ██████╔╝██║  ██║██║  ██║    ╚██████╔╝███████╗██║ ╚████║')
+  print(Fore.BLUE + '      ╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝')
+  print(Fore.BLUE + '                  Made by knq#1111')
+  print()
+  print()
+  print(Fore.GREEN + '[1] Generator')
+  print(Fore.CYAN + '[2] How to use FA Accounts ')
+  print(Fore.BLUE + '[3] Credits')
+  print(Fore.RED + '[4] Quit')
+  print('')
+
+  a = input(Fore.RED + '>> ')
+
+  if a == ('3'):
+    print('')
+    print(Fore.YELLOW + '       MAIN DEVELOPER: knq#1111')
+    print('')
+    print(Fore.BLUE + '       Helpers: Grogu#6969, eth#0001')
+    print(Fore.CYAN + '       Others: Skulldugger#2558, Again#8451, not xmister 18#1337, CraftBlox#9999')
+    print(Fore.GREEN + '       Good Suggestions: ✨CorruptedFbi#0456 ')
+    time.sleep(5)
+    os.system('cls')
+    return main1()
+
+
+  if a == ('2'):
+    os.system('cls')
+
+    print('')
+    print(Fore.BLUE + '      ██╗  ██╗██╗   ██╗██████╗ ██████╗  █████╗      ██████╗ ███████╗███╗   ██╗')
+    print(Fore.BLUE + '      ██║  ██║╚██╗ ██╔╝██╔══██╗██╔══██╗██╔══██╗    ██╔════╝ ██╔════╝████╗  ██║')
+    print(Fore.BLUE + '      ███████║ ╚████╔╝ ██║  ██║██████╔╝███████║    ██║  ███╗█████╗  ██╔██╗ ██║')
+    print(Fore.BLUE + '      ██╔══██║  ╚██╔╝  ██║  ██║██╔══██╗██╔══██║    ██║   ██║██╔══╝  ██║╚██╗██║')
+    print(Fore.BLUE + '      ██║  ██║   ██║   ██████╔╝██║  ██║██║  ██║    ╚██████╔╝███████╗██║ ╚████║')
+    print(Fore.BLUE + '      ╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝')
+    print(Fore.BLUE + '                  Made by knq#1111')
+    print()
+    print()
+    import webbrowser
+
+    webbrowser.open("https://www.youtube.com/watch?v=EvgypPSfCM4&ab_channel=GarvGamingYT444")
+    time.sleep(5)
+    return main1()
+  
+  if a == ('1'):
+      os.system('cls')
+      print('')
+      print(Fore.BLUE + '      ██╗  ██╗██╗   ██╗██████╗ ██████╗  █████╗      ██████╗ ███████╗███╗   ██╗')
+      print(Fore.BLUE + '      ██║  ██║╚██╗ ██╔╝██╔══██╗██╔══██╗██╔══██╗    ██╔════╝ ██╔════╝████╗  ██║')
+      print(Fore.BLUE + '      ███████║ ╚████╔╝ ██║  ██║██████╔╝███████║    ██║  ███╗█████╗  ██╔██╗ ██║')
+      print(Fore.BLUE + '      ██╔══██║  ╚██╔╝  ██║  ██║██╔══██╗██╔══██║    ██║   ██║██╔══╝  ██║╚██╗██║')
+      print(Fore.BLUE + '      ██║  ██║   ██║   ██████╔╝██║  ██║██║  ██║    ╚██████╔╝███████╗██║ ╚████║')
+      print(Fore.BLUE + '      ╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝')
+      print(Fore.BLUE + '                          Made by knq#1111')
+      print()
+      print()
+      print(Fore.RED + 'Options: ')
+      print()
+      print()
+      print(Fore.YELLOW + '  [1] Minecraft')
+      print(Fore.CYAN + '  [2] Uplay')
+      print(Fore.BLUE + '  [3] Valorant Skinned')
+      print(Fore.RED + '  [4] NordVPN')
+      print(Fore.YELLOW + '  [5] Paypal NFA')
+      print(Fore.BLUE + '  [6] Spotify')
+      print(Fore.CYAN + '  [7] Netflix')
+      print(Fore.GREEN + '  [8] Combolist')
+      print()
+      print()
+      b = input(Fore.RED + '>> ')
+
+      if b == ('8'):
+        print('')
+        print(Fore.YELLOW + '[1] Gaming [ Minecraft, Roblox, Fortnite ]')
+        print(Fore.YELLOW + '[2] Streaming [ Netflix, HBO ]')
+        print(Fore.YELLOW + '[3] VPN [ NordVPN, VyperVPN, TunnelBear ]')
+        print(Fore.YELLOW + '[4] Mail Access [ Mc FA, Blockchain FA, etc..')
+        print('')
+        cbl = input(Fore.RED + '>> ')
+        if cbl == ('1'):
+          cblg = URL = "https://pastehub.net/raw.php?p=f7e7a0ca7f1"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your combolist has been generated, save the link. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+
+        if cbl == ('2'):
+          cbls = URL = "https://pastehub.net/raw.php?p=f810f32bb9a"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your combolist has been generated, save the link. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+
+        if cbl == ('3'):
+          cblv = URL = "https://pastehub.net/raw.php?p=f80fc21d883"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your combolist has been generated, save the link. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+
+        if cbl == ('4'):
+          cbls = URL = "https://pastehub.net/raw.php?p=f8109abd80d"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your combolist has been generated, save the link. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+
+      if b == ('1'):
+        os.system('cls')
+        print('')
+        print(Fore.BLUE + '      ██╗  ██╗██╗   ██╗██████╗ ██████╗  █████╗      ██████╗ ███████╗███╗   ██╗')
+        print(Fore.BLUE + '      ██║  ██║╚██╗ ██╔╝██╔══██╗██╔══██╗██╔══██╗    ██╔════╝ ██╔════╝████╗  ██║')
+        print(Fore.BLUE + '      ███████║ ╚████╔╝ ██║  ██║██████╔╝███████║    ██║  ███╗█████╗  ██╔██╗ ██║')
+        print(Fore.BLUE + '      ██╔══██║  ╚██╔╝  ██║  ██║██╔══██╗██╔══██║    ██║   ██║██╔══╝  ██║╚██╗██║')
+        print(Fore.BLUE + '      ██║  ██║   ██║   ██████╔╝██║  ██║██║  ██║    ╚██████╔╝███████╗██║ ╚████║')
+        print(Fore.BLUE + '      ╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝')
+        print(Fore.BLUE + '                          Made by knq#1111')
+        print()
+        print()
+        print(Fore.RED + 'Options: ')
+        print()
+        print()
+        print(Fore.YELLOW + '  [1] NFA')
+        print(Fore.YELLOW + '  [2] SFA [ Unchecked ]')
+        print(Fore.YELLOW + '  [3] FA [ 70% Working ]')
+        c = input(Fore.RED + '>> ')
+        if c == ('1'):
+          print(Fore.BLUE + 'Your NFA account is:  ')
+          URL = "https://pastehub.net/raw.php?p=f6a190213eb"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your account has been generated. Copy the credentials. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+        if c == ('2'):
+          print(Fore.BLUE + 'Your SFA account is:  ')
+          URL = "https://pastehub.net/raw.php?p=f57706d64e5"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your account has been generated. Copy the credentials. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+        if c == ('3'):
+          print(Fore.BLUE + 'Your FA account is:  ')
+          URL = "https://pastehub.net/raw.php?p=f5776d71fc8"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your account has been generated. Copy the credentials. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+
+      if b == ('2'):        
+          print(Fore.BLUE + 'Your Uplay account is:  ')
+          URL = "https://pastehub.net/raw.php?p=f577d629d31"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your account has been generated. Copy the credentials. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+      if b == ('3'):        
+          print(Fore.BLUE + 'Your Valorant account is:  ')
+          URL = "https://pastehub.net/raw.php?p=f57835061f2"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your account has been generated. Copy the credentials. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+      if b == ('4'):        
+          print(Fore.BLUE + 'Your NordVPN account is:  ')
+          URL = "https://pastehub.net/raw.php?p=f5789462fd9"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your account has been generated. Copy the credentials. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+      if b == ('5'):        
+          print(Fore.BLUE + 'Your Paypal account is:  ')
+          URL = "https://paste.gg/p/anonymous/100a24c458f7473badbe307e3e18da07/files/aaad8acde595486882cd2be40c839199/raw"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your account has been generated. Copy the credentials. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+      if b == ('6'):        
+          print(Fore.BLUE + 'Your Spotify account is:  ')
+          URL = "https://pastehub.net/raw.php?p=f81788cd1fe"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your account has been generated. Copy the credentials. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+      if b == ('7'):        
+          print(Fore.BLUE + 'Your Netflix account is:  ')
+          URL = "https://pastehub.net/raw.php?p=f8156cb037f"
+
+          contents = requests.get(URL).text.split('\n')
+          print(Fore.BLUE + random.choice(contents).strip())
+          print('')
+          print(Fore.YELLOW + '[!] Your account has been generated. Copy the credentials. You will be returned to the main menu in 15 seconds.')
+          time.sleep(15)
+          os.system('cls')
+          return main1()
+          
+
+          
+        
+        
+
+main1()
